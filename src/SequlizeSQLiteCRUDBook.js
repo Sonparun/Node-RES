9f86fcf9133d6acb1282dc5d17ae520fa70c1cda
Sonparun <109581702+Sonparun@users.noreply.github.com>
2023-09-19 11:12:44 +0700

    fix

--- src/SequlizeSQLiteCRUDBook.js
+++ src/SequlizeSQLiteCRUDBook.js
@@ -1,88 +1,94 @@
-const express=require('express');
-const Sequelize=require('sequelize');
-const app=express();
+const express = require('express');
+const Sequelize = require('sequelize');
+const app = express();
 
 app.use(express.json());
 
-const sequelize=new Sequelize('database','username','password',{
-    host: 'localhost',
-    dialect: 'sqlite',
-    storage: './Database/SQBooks.sqlite'
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'sqlite',
+  storage: './Database/SQBooks.sqlite',
 });
 
-const Book=sequelize.define('book',{
-    id: {
+const Book = sequelize.define('book', {
+  id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
-    primaryKey: true
-},
-    title: {
+    primaryKey: true,
+  },
+  title: {
     type: Sequelize.STRING,
-    allowNUll: false
-},
-    author: {
+    allowNull: false, // แก้จาก allowNUll เป็น allowNull
+  },
+  author: {
     type: Sequelize.STRING,
-    allowNUll: false}
+    allowNull: false, // แก้จาก allowNUll เป็น allowNull
+  },
 });
 
 sequelize.sync();
 
-app.get('/books',(req,res)=>{
-    Book.findAll().then(books=>{
-        res.json(books);
-    }).catch(err=>{res.status(500).send(err);
+app.get('/books', (req, res) => {
+  Book.findAll()
+    .then((books) => {
+      res.json(books);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
     });
 });
 
-app.get('/books/:id',(req,res)=>{
-    Book.findByPk(req.params.id).then(book=>{
-        if(!book){
-            res.status(404).send('Book not found');
-        }else{res.json(book);
-        }
-    }).catch(err=>{
-        res.status(500).send(err);
+app.get('/books/:id', (req, res) => {
+  Book.findByPk(req.params.id)
+    .then((book) => {
+      if (!book) {
+        res.status(404).send('Book not found');
+      } else {
+        res.json(book);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send(err);
     });
 });
 
-app.post('/books',(req,res)=>{
-    Book.create(req.body).then(book=>{
-        res.send(book);
-    }).catch(err=>{
-        res.status(500).send(err);
+app.post('/books', (req, res) => {
+  Book.create(req.body)
+    .then((book) => {
+      res.send(book);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
     });
 });
 
-app.put('/books/:id',(req,res)=>{
-    Book.findByPk(req.param.id).then(book=>{
-        if(!book){
-            res.status(404).send('Book not found');
-        }else {book.update(req.body).then(() =>{
-            res.send(book);
-        }).catch(err=>{
-            res.status(500).send(err);
-        });
+app.put('/books/:id', async (req, res) => { // เพิ่ม async และ await ในการใช้งาน
+  try {
+    const book = await Book.findByPk(req.params.id);
+    if (!book) {
+      res.status(404).send('Book not found');
+    } else {
+      await book.update(req.body);
+      res.send(book);
     }
-        }).catch(err=>{
-            res.status(500).send(err);
-        });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-app.delete('/books/:id',(req,res)=>{
-    Book.findByPk(req.params.id).then(book=>{
-        if(!book){
-            res.status(404).send('Book not found');
-        }else{book.destroy().then(()=>{
-            res.send({});
-        }).catch(err=>{
-            res.status(500).send(err);
-        });
+app.delete('/books/:id', async (req, res) => { // เพิ่ม async และ await ในการใช้งาน
+  try {
+    const book = await Book.findByPk(req.params.id);
+    if (!book) {
+      res.status(404).send('Book not found');
+    } else {
+      await book.destroy();
+      res.send({});
     }
-    }).catch(err=>{
-        res.status(500).send(err);
-    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-const port=process.env.PORT||3000;
-app.listen(port,()=>console.log(`Listening on port ${port}...`));
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
